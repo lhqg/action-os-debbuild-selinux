bcad258b5ca8802b8965e33ac6f01214e5fd17a2
Hubert Quarantel-Colombani <hubert@quarantel.name>
2024-09-30 15:38:31 +0200

    Using /etc/os-release file for distro
    Signed-off-by: Hubert Quarantel-Colombani <hubert@quarantel.name>

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,28 @@
 # syntax=docker/dockerfile:1
 # see https://docs.docker.com/engine/reference/builder/#understand-how-arg-and-from-interact
-ARG DISTRIBUTION=ubuntu:latest
+ARG DISTRIBUTION=ubuntu
+ARG DISTRO_VERSN=latest
 ARG PLATFORM=amd64
 
-FROM --platform=linux/${PLATFORM} ${DISTRIBUTION}
+FROM --platform=linux/${PLATFORM} ${DISTRIBUTION}:${DISTRO_VERSN}
 
 # set the environment variables that gha sets
-ENV INPUT_DISTRIBUTION="${DISTRIBUTION}"
-ENV INPUT_PLATFORM="${PLATFORM}"
-ENV INPUT_RESULT_DIR="artifacts"
+ENV INPUT_BUILDROOT=""
+ENV INPUT_PKG_VERSION=""
+ENV INPUT_OUTPUT_DIR="artifacts"
+ENV INPUT_SIGNING_KEY_NAME=""
+ENV INPUT_SIGNING_KEY_ID=""
+ENV INPUT_SIGNING_KEY_FILE=""
 
 # Set the env variables to non-interactive
-ENV DEBIAN_FRONTEND noninteractive
-ENV DEBIAN_PRIORITY optional
-ENV DEBCONF_NOWARNINGS yes
+ENV DEBIAN_FRONTEND=noninteractive
+ENV DEBIAN_PRIORITY=optional
+ENV DEBCONF_NOWARNINGS=yes
 
 # Install build environment
-RUN apt-get update
-RUN apt-get -y install selinux-basics selinux-policy-dev policycoreutils build-essential debhelper dh-make devscripts
+RUN apt-get update -yqq && apt-get install -yqq --no-install-recommends \
+	selinux-basics selinux-policy-dev policycoreutils \
+	build-essential debhelper dh-make devscripts
 
 COPY ./build.sh .
 
--- build.sh
+++ build.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-DISTRO=$( cut -d : -f 1 <<< "${INPUT_DISTRIBUTION}" )
-DISTRO_NAMEVER=$( cut -d : -f 2 <<< "${INPUT_DISTRIBUTION}" )
-PLATFORM="${INPUT_PLATFORM}"
-BUILDROOT="${INPUT_BUILDROOT}"
-OUTPUT_DIR="${INPUT_OUTPUT_DIR}"
-GPG_KEY_NAME="${INPUT_GPG_KEY_NAME}"
-GPG_KEY_FILE="${INPUT_GPG_KEY_FILE}"
-PLATFORM="${INPUT_PLATFORM}"
-
 RC=0
 
+WORKDIR=/source
+LOCKFILE=${WORKDIR}/.lock
+
+BUILDROOT="${WORKDIR}/${INPUT_BUILDROOT}"
+OUTPUT_DIR="${WORKDIR}/build/${INPUT_OUTPUT_DIR}"
+PACKAGE_VERSION="${INPUT_PKG_VERSION}"
+GPG_KEY_NAME="${INPUT_SIGNING_KEY_NAME}"
+GPG_KEY_ID="${INPUT_SIGNING_KEY_ID}"
+GPG_KEY_FILE="${INPUT_SIGNING_KEY_FILE}"
+
+if [ -f ${LOCKFILE} ]
+then
+  count=0
+  waittime=20
+  while [ $count -lt 10 -a -f $LOCKFILE ]
+  do
+    echo "::notice title=DEBbuild::Lock file found, waiting ${waittime}s"
+    sleep $waittime
+    count=$((count + 1))
+  done
+  if [ -f ${LOCKFILE}]
+  then
+    echo "::error title=DEBbuild::Lock still present, aborting."
+    exit 127
+  fi
+fi
+
+touch $LOCKFILE
+
 pwd
+
+[ -d ${OUTPUT_DIR} ] || mkdir -m 0755 -p ${OUTPUT_DIR}
 
 if [ -n "${BUILDROOT}" -a -d "${BUILDROOT}" ]
 then
@@ -24,46 +46,61 @@ then
 
   old_pwd=$( pwd )
   export GPG_TTY=$(tty)
+  export GNUPGHOME=${BUILDROOT}/.gnupg
 
   if [ $RC -eq 0 ]
   then
     # Preparing to sign DEB packages
     if [ -s ${BUILDROOT}/${GPG_KEY_FILE} -a -s ${BUILDROOT}/.${GPG_KEY_FILE}.passphrase ] 
     then
+      mkdir -m 0700 -p ${GNUPGHOME}
       gpg --batch \
         --passphrase-file ${BUILDROOT}/.${GPG_KEY_FILE}.passphrase \
         --import ${BUILDROOT}/${GPG_KEY_FILE}
       rc1=$?
+      sign_options=""
       [ $rc1 -ne 0 ] && echo "::error title=DEBbuild::Could not import private key."
+      [ $rc1 -eq 0 ] && sign_options="--force-sign"
     fi
 
     # Loop over all CONTROL files found
     for control in ${BUILDROOT}/*/debian/control
     do
       package_dir=$( dirname $( dirname $control ) )
+      package_name=$( basename ${package_dir} )
 
-      echo "::notice title:DEBbuild:Entering ${package_dir}."
+      echo "::notice title:DEBbuild:Entering ${package_dir}"
       cd ${package_dir}
-      debuild
-      RC=$(($RC + $? ))
+      dpkg-buildpackage -v${PACKAGE_VERSION} --jobs=1 --build=binary ${sign_options}
+      rc2=$?
+      RC=$(($RC + $rc2 ))
 
-      if [ $RC -eq 0 ]
+      if [ $rc2 -eq 0 ]
       then
-        mv ${BUILDROOT}/*.{deb,changes} ${OUTPUT_DIR}/
+        echo "::notice title:DEBbuild:Listing package content."
+        dpkg-deb -c ${BUILDROOT}/*.deb 
+
+        echo "::notice title:DEBbuild:Moving files to ${OUTPUT_DIR}"
+        mv ${BUILDROOT}/*.deb ${OUTPUT_DIR}/
         RC=$(($RC + $? ))
+
+        mv ${BUILDROOT}/*.{changes,dsc} ${OUTPUT_DIR}/
       fi
 
       cd $old_pwd
     done
 
     # Rename generated files with Distro name and version
-    cd ${OUTPUT_DIR}
-    for file in *
-      mv ${file} ${file}-${DISTRO}_${DISTRO_NAMEVER}
+    if [ $RC -eq 0 ]
+    then
+      cd ${OUTPUT_DIR}
+      . /etc/os-release
+      find . -type f -a \
+        \( -name '*.deb' -o -name '*.changes' -o -name '*.buildinfo' \) \
+        -print0 | xargs -0 \
+        tar cfj ${OUTPUT_DIR}/${ID}-${VERSION_ID}.tar.bz2
       RC=$(($RC + $? ))
-    do
-
-    done
+    fi
   else
     echo "::error title=DEBbuild::Could not find any CONTROL file in ${BUILDROOT}/*/debian/ directories"
     RC=2
@@ -71,7 +108,9 @@ then
 
   RC=$(( $RC + $rc1 ))
 else
-  echo "::error title=DEBbuild::Unable to find the root build directory."
+  echo "::error title=DEBbuild::Unable to find the root build directory '${BUILDROOT}'."
 fi
+
+rm -f $LOCKFILE
 
 exit $RC
